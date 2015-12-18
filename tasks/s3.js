8b2c8fe480aec9a225c53cb5c42b695087694c07
Grant Craig <grant@getfoodgenius.com>
2015-12-18 16:10:37 -0600

    update

--- tasks/s3.js
+++ tasks/s3.js
@@ -14,25 +14,12 @@ function Task(conf, allConf, callback) {
   this.conf = conf;
   this.allConf = allConf;
 
-  if (this.conf.requiresTag && !tag) {
-    gutil.log("TAG must be set in the ENV");
-    process.exit(1);
-  }
-
   const options = {
     uploadPath: this.conf.uploadPath,
     gzippedOnly: this.conf.gzip || false
   };
 
   gulp.src(this.conf.files)
-  .pipe(gulpif(this.conf.gzip, gzip()))
-  .pipe(s3(aws, options));
-
-  gulp.src(this.conf.files)
-  .pipe(gulpif(this.conf.requiresTag, rename(function (path) {
-    path.basename = path.basename + "-" + tag;
-    return path;
-  })))
   .pipe(gulpif(this.conf.gzip, gzip()))
   .pipe(s3(aws, options));
 
